b53da3904d59ed09402ce64e55b8b3b3f141e1d7
adutlersaar <adutler.saar@gmail.com>
2022-08-05 20:08:04 +0300

    save

--- experiments_settings.py
+++ experiments_settings.py
@@ -9,6 +9,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import matthews_corrcoef, accuracy_score, roc_auc_score, make_scorer
 from utils import pr_auc
+from mrmr import mrmr_classif
 
 MODELS = {'nb': GaussianNB(),
           'svm': SVC(kernel='rbf'),
@@ -16,7 +17,10 @@ MODELS = {'nb': GaussianNB(),
           'rf': RandomForestClassifier(),
           'knn': KNeighborsClassifier()}
 
-FEATURES_SELECTORS = {'fdr': SelectFdr(alpha=0.1)}
+FEATURES_SELECTORS = {'fdr': SelectFdr(alpha=0.1),
+                      'mrmr': lambda x: mrmr_classif(x_train, y_train, K=n-i, show_progress=False),
+                      'rfe_svm': '',
+                      'reliefF'}
 
 KS = [1, 2, 3, 4, 5, 10, 15, 20, 25, 30, 50, 100]
 
@@ -24,11 +28,11 @@ DATASETS_FILES = list(map(str, Path('data/preprocessed').glob('*.csv')))
 
 # binary and multiclass metrics
 METRICS_B = {'roc_auc': lambda y_true, y_score: roc_auc_score(y_true, y_score[:, 1]),
-           'acc': lambda y_true, y_score: accuracy_score(y_true, np.argmax(y_score, axis=1)),
-           'mcc': lambda y_true, y_score: matthews_corrcoef(y_true, np.argmax(y_score, axis=1)),
-           'pr_auc': lambda y_true, y_score: pr_auc(y_true, y_score[:, 1])}
+             'acc': lambda y_true, y_score: accuracy_score(y_true, np.argmax(y_score, axis=1)),
+             'mcc': lambda y_true, y_score: matthews_corrcoef(y_true, np.argmax(y_score, axis=1)),
+             'pr_auc': lambda y_true, y_score: pr_auc(y_true, y_score[:, 1])}
 
 METRICS_M = {'roc_auc': lambda y_true, y_score: roc_auc_score(y_true, y_score, average='weighted', multi_class='ovr'),
-           'acc': lambda y_true, y_score: accuracy_score(y_true, np.argmax(y_score, axis=1)),
-           'mcc': lambda y_true, y_score: matthews_corrcoef(y_true, np.argmax(y_score, axis=1)),
-           'pr_auc': pr_auc}
+             'acc': lambda y_true, y_score: accuracy_score(y_true, np.argmax(y_score, axis=1)),
+             'mcc': lambda y_true, y_score: matthews_corrcoef(y_true, np.argmax(y_score, axis=1)),
+             'pr_auc': pr_auc}
