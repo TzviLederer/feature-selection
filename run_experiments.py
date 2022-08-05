04c70c74ef54d4eee14a2441d221e84d46341b18
adutlersaar <adutler.saar@gmail.com>
2022-08-05 21:40:47 +0300

    restored per fold logging

--- run_experiments.py
+++ run_experiments.py
@@ -26,7 +26,7 @@ def run_experiment(estimator_name, filename, filtering_algo, num_selected_featur
     df = pd.read_csv(filename)
     cv = get_cv(df)
     features_selector = FEATURES_SELECTORS[filtering_algo](num_selected_features)
-    preprocessor = DataPreprocessor()
+    METRICS = METRICS_B if len(set(df[LABEL_COL])) == 2 else METRICS_M
 
     log_filename = f'{"_".join([estimator_name, Path(filename).name, filtering_algo, str(num_selected_features)])}.json'
     if logs_dir:
@@ -37,58 +37,54 @@ def run_experiment(estimator_name, filename, filtering_algo, num_selected_featur
         print('Exists, skipping')
         return log_filename
 
-    log_experiment_params = {
-        'learning_algo': estimator_name,
-        'dataset': Path(filename).name,
-        'filtering_algo': filtering_algo,
-        'n_selected_features': num_selected_features,
-        'cv_method': str(cv),
-        'n_samples': df.shape[0],
-        'n_features_org': df.shape[1]
-    }
+    exp_logs = []
+    for train_index, val_index in cv.split(df, df['y']):
+        metrics, stats = train_and_evaluate(estimator, features_selector, num_selected_features, df[train_index],
+                                            df[val_index], METRICS)
 
-    # preprocessing
-    preprocessor.fit(df)
-    df = preprocessor.transform(df)
-    METRICS = METRICS_B if len(set(df[LABEL_COL])) == 2 else METRICS_M
+        base_log = {
+            'learning_algo': estimator_name,
+            'dataset': Path(filename).name,
+            'filtering_algo': filtering_algo,
+            'n_selected_features': num_selected_features,
+            'cv_method': str(cv),
+            'n_samples': df.shape[0],
+            'n_features_org': df.shape[1],
+            **stats
+        }
+        exp_logs.extend([{**base_log, metric: value} for metric, value in metrics.items()])
 
-    # feature selection
-    fs_fit_time, k_best, fs_out_scores = get_k_best(df, features_selector, num_selected_features)
-
-    # evaluate results
-    mean_fit_time, probas, mean_pred_time = fit_estimator(cv, df, estimator, k_best)
-    metrics = {metric: metric_func(df[LABEL_COL], probas) for metric, metric_func in METRICS.items()}
-    log_outputs = {'fit_time': mean_fit_time,
-                   'fs_fit_time': fs_fit_time,
-                   'selected_features_names': k_best,
-                   'selected_features_scores': fs_out_scores,
-                   **log_experiment_params,
-                   **metrics}
     with open(log_filename, 'w') as f:
-        json.dump(log_outputs, f)
+        json.dump(exp_logs, f)
 
     return log_filename
 
 
-def fit_estimator(cv, df, estimator, k_best):
-    probas = []
-    fit_times = []
-    predict_time = .0
-    for train_index, val_index in cv.split(df, df['y']):
-        t0 = time.time()
-        estimator.fit(df.iloc[train_index][k_best], df.iloc[train_index][LABEL_COL])
-        fit_times.append(time.time() - t0)
+def train_and_evaluate(estimator, features_selector, num_selected_features, train_df, val_df, metrics_funcs):
+    # preprocessing
+    preprocessor = DataPreprocessor()
+    preprocessor.fit(train_df)
+    train_df = preprocessor.transform(train_df)
+    val_df = preprocessor.transform(val_df)
+    # feature selection
+    fs_time, k_best, k_best_scores = get_k_best_features(train_df, features_selector, num_selected_features)
+    # fit estimator
+    t0 = time.time()
+    estimator.fit(train_df[k_best], train_df[LABEL_COL])
+    fit_time = time.time() - t0
+    # evaluate
+    t0 = time.time()
+    probas = estimator.predict_proba(val_df[k_best])
+    predict_time = (time.time() - t0)
+    metrics = {metric: func(val_df[LABEL_COL], probas) for metric, func in metrics_funcs.items()}
+    return metrics, {'fit_time': fit_time,
+                     'feature_selection_time': fs_time,
+                     'predict_time': predict_time,
+                     'selected_features_names': k_best,
+                     'selected_features_scores': k_best_scores}
 
-        t0 = time.time()
-        probas.append(estimator.predict_proba(df.iloc[val_index][k_best]))
-        predict_time += (time.time() - t0)
-    mean_fit_time = np.array(fit_times).mean()
-    probas = np.concatenate(probas, axis=0)
-    mean_pred_time = predict_time / len(df)
-    return mean_fit_time, probas, mean_pred_time
 
-
-def get_k_best(df, features_selector, k):
+def get_k_best_features(df, features_selector, k):
     X, y = df.drop(LABEL_COL, axis=1), df[LABEL_COL]
     # feature selection fit
     start_time = time.time()
