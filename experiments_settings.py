15c7faba09397b0d13b2b42af6e1f1fbbc6a4f91
tzvilederer <zvi.lederer@gmail.com>
2022-08-08 13:28:21 +0300

    try to fix cachedir warning

--- experiments_settings.py
+++ experiments_settings.py
@@ -39,9 +39,6 @@ METRICS_M = {'roc_auc': make_scorer(roc_auc_score, average='weighted', multi_cla
 
 N_JOBS = -1
 
-CACHEDIR = 'cachedir'
-Path(CACHEDIR).mkdir(exist_ok=True)
-
 
 def get_cv(X):
     if len(X) < 50:
