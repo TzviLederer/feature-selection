716376f5e168d646b618d8a2c028c24e15af154a
adutlersaar <adutler.saar@gmail.com>
2022-08-15 17:18:58 +0300

    implemented two phase mutation

--- grey_wolf_fs.py
+++ grey_wolf_fs.py
@@ -15,27 +15,48 @@ def one_wolf_fitness(X, y, wolf, alpha):
     return alpha * error_rate + (1 - alpha) * wolf.mean()
 
 
-def two_phase_mutation(x):
-    return x
+def two_phase_mutation(X, y, wolf, fitness, alpha, mutation_prob):
+    wolf_mutated = wolf.copy()
+    one_positions = np.argwhere(wolf == 1).T[0]
+    for i in one_positions:
+        r = np.random.rand()
+        if r < mutation_prob:
+            wolf_mutated[i] = 0
+            mutated_fitness = one_wolf_fitness(X, y, wolf_mutated, alpha)
+            if mutated_fitness < fitness:
+                fitness = mutated_fitness
+                wolf = wolf_mutated
+
+    wolf_mutated = wolf.copy()
+    zero_positions = np.argwhere(wolf == 0).T[0]
+    for i in zero_positions:
+        r = np.random.rand()
+        if r < mutation_prob:
+            wolf_mutated[i] = 1
+            mutated_fitness = one_wolf_fitness(X, y, wolf_mutated, alpha)
+            if mutated_fitness < fitness:
+                fitness = mutated_fitness
+                wolf = wolf_mutated
+
+    return wolf
 
 
-def calculate_fitnesses(X, y, wolfs, alpha, two_phase=True):
+def calculate_fitnesses(X, y, wolfs, alpha, two_phase_mutation_prob=None):
     fitnesses = [one_wolf_fitness(X, y, wolf, alpha) for wolf in wolfs]
 
-    if two_phase:
+    if two_phase_mutation_prob:
         alpha_idx = np.argmax(fitnesses)
-        new_x_alpha = two_phase_mutation(wolfs[alpha_idx])
-        wolfs[alpha_idx] = new_x_alpha
-        fitnesses[alpha_idx] = one_wolf_fitness(X, y, new_x_alpha, alpha)
+        new_x_alpha = two_phase_mutation(X, y, wolfs[alpha_idx], fitnesses[alpha_idx], alpha, two_phase_mutation_prob)
+        wolfs[alpha_idx], fitnesses[alpha_idx] = new_x_alpha, one_wolf_fitness(X, y, new_x_alpha, alpha)
 
     return fitnesses
 
 
-def grey_wolf_fs(X, y, n_agents=10, iterations=100, alpha=0.999):
+def grey_wolf_fs(X, y, n_agents=10, iterations=100, alpha=0.999, two_phase_mutation_prob=0.5):
     wolfs = (np.random.rand(n_agents, len(X.columns)) > .5).astype(float)
     n = len(X.columns)
 
-    fitnesses = calculate_fitnesses(X, y, wolfs, alpha, two_phase=False)
+    fitnesses = calculate_fitnesses(X, y, wolfs, alpha)
     sorted_index = np.argsort(fitnesses)
     x_alpha, x_beta, x_delta = [wolfs[i] for i in sorted_index[:3]]
 
@@ -60,7 +81,7 @@ def grey_wolf_fs(X, y, n_agents=10, iterations=100, alpha=0.999):
         x_binary = np.random.rand(*wolfs.shape) >= x_si
         wolfs[:] = x_binary
 
-        fitnesses = calculate_fitnesses(X, y, wolfs, alpha)
+        fitnesses = calculate_fitnesses(X, y, wolfs, alpha, two_phase_mutation_prob=two_phase_mutation_prob)
         sorted_index = np.argsort(fitnesses)
         x_alpha, x_beta, x_delta = [wolfs[i] for i in sorted_index[:3]]
 
@@ -71,11 +92,11 @@ def grey_wolf_fs(X, y, n_agents=10, iterations=100, alpha=0.999):
     return min_fitness_x_alpha
 
 
-def grey_wolf_n_layered_fs(X, y, n_agents=10, iterations=100, alpha=0.999, n_layers=5):
+def grey_wolf_n_layered_fs(X, y, n_agents=10, iterations=100, alpha=0.999, two_phase_mutation_prob=0.5, n_layers=5):
     wolfs = (np.random.rand(n_agents, len(X.columns)) > .5).astype(float)
     n = len(X.columns)
 
-    fitnesses = calculate_fitnesses(X, y, wolfs, alpha, two_phase=False)
+    fitnesses = calculate_fitnesses(X, y, wolfs, alpha)
     sorted_index = np.argsort(fitnesses)
     x_bests_wolves = [wolfs[i] for i in sorted_index[:n_layers]]
 
@@ -99,7 +120,7 @@ def grey_wolf_n_layered_fs(X, y, n_agents=10, iterations=100, alpha=0.999, n_lay
         x_binary = np.random.rand(*wolfs.shape) >= x_si
         wolfs[:] = x_binary
 
-        fitnesses = calculate_fitnesses(X, y, wolfs, alpha)
+        fitnesses = calculate_fitnesses(X, y, wolfs, alpha, two_phase_mutation_prob=two_phase_mutation_prob)
         sorted_index = np.argsort(fitnesses)
         x_bests_wolves = [wolfs[i] for i in sorted_index[:n_layers]]
 
